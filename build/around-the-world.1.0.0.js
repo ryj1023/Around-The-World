d826247adc069c218a122b2f8466aebd892f2e17
Ryan Johnson <ryj1023@yahoo.com>
2017-01-09 21:32:12 -0500

    added CDNs and moved source files for github server

--- build/around-the-world.1.0.0.js
+++ build/around-the-world.1.0.0.js
@@ -121,7 +121,6 @@
 				(function () {
 					var ctrl = _this;
 					GoogleLocation.getLocation(ctrl.locTag, function (response) {
-						console.log(response);
 						if (response.data.status == "ZERO_RESULTS") {
 							alert("No results. Please select a new location.");
 						} else {
